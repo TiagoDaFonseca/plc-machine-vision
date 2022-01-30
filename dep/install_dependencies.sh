b4439addac160452dc5a958245408460f7f4581e
Tiago Cunha <30734378+TiagoDaFonseca@users.noreply.github.com>
2022-01-30 16:04:25 +0000

    Create install_dependencies.sh

---
+++ dep/install_dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+apt-get update # to get the most recent packages updates
+
+#########################################################
+#Packages List to install
+#
+apt-get install libblas-dev
+ap-get install liblapack-dev
+apt-get install libxml2
+apt-get install libxslt1-dev
+apt-get install zlib1g-dev
+apt-get install libv4l-dev
+apt-get install v4l-utils
+apt-get install tesseract-ocr
+apt-get install libopencv-dev
+apt-get install python-opencv
+
+#########################################################
+# EOF
+
